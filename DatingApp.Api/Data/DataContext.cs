b4d30b035850afe1a21c332f300242af04dbaef0
Chika Ejiofor <chikaejiofor95@yahoo.com>
2020-08-01 03:29:53 +0100

    Initail change

---
+++ DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.Api.MOdels;
+
+
+namespace DatingApp.Api.Data
+
+{
+    public class DataContext : DbContext
+    {
+         public DataContext(DbContextOptions<DataContext> options) : base (options)
+         {
+         }
+
+         public DbSet<Value> Values {get; set;}
+    }
+}
